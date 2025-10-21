9beacf52b20c3f2bab86463b02ace9a2e26f4091
Suleman Hafeez <sulemanhafeezmuhammad@gmail.com>
2025-10-21 19:04:32 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/Portfolio-Website/", // ✅ Add this for GitHub Pages
   plugins: [
     react(),
     runtimeErrorOverlay(),
